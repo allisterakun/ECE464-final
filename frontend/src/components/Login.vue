4a8e5db7c0c71b99e45d607092c97ed700015e48
BonnyWang <bonny.y.gardy@gmail.com>
2021-12-03 19:57:18 -0500

    Added bootstrap vue and login

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <input v-model="message" placeholder="edit me">
+</template>
+
+<script>
+export default {
+    name:"Login",
+    data:function(){
+        return{
+            message:""
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
