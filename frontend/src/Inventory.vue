cc60a8cbbc4eeb3f8fec99b4827929244fcc3460
BonnyWang <bonny.y.gardy@gmail.com>
2021-12-11 17:39:01 -0500

    inventory half done

--- frontend/src/Inventory.vue
+++ frontend/src/Inventory.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
+        <b-dropdown style="width:fit-content" id="dropdown-1" text="Type" class="m-2">
+            <b-dropdown-item>All</b-dropdown-item>
+            <b-dropdown-item>Fruit</b-dropdown-item>
+            <b-dropdown-item>Meat</b-dropdown-item>
+            <b-dropdown-item>Drink</b-dropdown-item>
+        </b-dropdown>
+        <b-table striped hover :items="inventory"></b-table>
     </div>
   
 </template>
@@ -11,15 +18,31 @@ export default {
     name:"Inventory",
     methods:{
         getInventory(){
-            axios.get(backEndAddress + "/getInventory",params)
+            let self = this;
+            let parameters = {
+                params:{
+                    // item_type: type,
+                    store_id: this.$cookie.get("store_id")
+                }
+            }
+            axios.get(backEndAddress + "/getInventory",parameters)
             .then(res => {
+                self.inventory = res.data;
                 console.log(res)
             })
             .catch(err => {
                 console.error(err); 
             })
         }
-    }
+    },
+    mounted(){
+        this.getInventory();
+    },
+    data() {
+        return {
+            inventory:[]
+        }
+    },
 }
 </script>
 
