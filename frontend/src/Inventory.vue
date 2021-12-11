d0c71867bd054d5d8f33a7c1ced7d5a4e88eb5a7
BonnyWang <bonny.y.gardy@gmail.com>
2021-12-11 17:57:38 -0500

    fixed the drop down

--- frontend/src/Inventory.vue
+++ frontend/src/Inventory.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <b-dropdown style="width:fit-content" id="dropdown-1" text="Type" class="m-2">
-            <b-dropdown-item>All</b-dropdown-item>
-            <b-dropdown-item>Fruit</b-dropdown-item>
-            <b-dropdown-item>Meat</b-dropdown-item>
-            <b-dropdown-item>Drink</b-dropdown-item>
+        <b-dropdown style="width:fit-content" id="dropdown-1" :text="currentType" class="m-2">
+            <b-dropdown-item v-on:click="getInventory('all')" >All</b-dropdown-item>
+            <b-dropdown-item v-on:click="getInventory('fruit')">Fruit</b-dropdown-item>
+            <b-dropdown-item v-on:click="getInventory('meat')">Meat</b-dropdown-item>
+            <b-dropdown-item v-on:click="getInventory('drink')">Drink</b-dropdown-item>
         </b-dropdown>
         <b-table striped hover :items="inventory"></b-table>
     </div>
@@ -17,14 +17,26 @@ import backEndAddress from './utili.js'
 export default {
     name:"Inventory",
     methods:{
-        getInventory(){
+        getInventory(type){
             let self = this;
-            let parameters = {
-                params:{
-                    // item_type: type,
-                    store_id: this.$cookie.get("store_id")
+            let parameters;
+            this.currentType = type;
+            if(type == 'all'){
+                parameters = {
+                    params:{
+                        // item_type: type,
+                        store_id: this.$cookie.get("store_id")
+                    }
+                }
+            }else{
+                parameters = {
+                    params:{
+                        item_type: type,
+                        store_id: this.$cookie.get("store_id")
+                    }
                 }
             }
+            
             axios.get(backEndAddress + "/getInventory",parameters)
             .then(res => {
                 self.inventory = res.data;
@@ -36,10 +48,11 @@ export default {
         }
     },
     mounted(){
-        this.getInventory();
+        this.getInventory('all');
     },
     data() {
         return {
+            currentType:"all",
             inventory:[]
         }
     },
