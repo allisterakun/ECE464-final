c8fe539e42dad7cea4a5f373ce90f0d2a05b7b7d
Brian <doan@cooper.edu>
2021-12-11 21:32:25 -0500

    updated employee naming

--- Backend/insert.sql
+++ Backend/insert.sql
@@ -4,10 +4,10 @@ insert into Stores values (1, "Key Food");
 insert into Stores values (2, "Giant Food");
 insert into Stores values (3, "Food Lion");
 
-insert into Employees values (1, "Bully Maguire", 1, "Cashier", 20);
+insert into Employees values (1, "Bully Maguire", 1, "Employee", 20);
 insert into Employees values (2, "Tom Cat", 1, "Manager", 30);
-insert into Employees values (3, "Ben Simmons", 1, "Cashier", 10);
-insert into Employees values (4, "Andrew Garfield", 2, "Cashier", 40);
+insert into Employees values (3, "Ben Simmons", 1, "Employee", 10);
+insert into Employees values (4, "Andrew Garfield", 2, "Employee", 40);
 insert into Employees values (5, "Peter Parker", 2, "Manager", 50);
 
 insert into Login_ values (1, "two", "weeks");
